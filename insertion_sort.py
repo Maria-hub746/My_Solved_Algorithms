58dc95d58541a58b8328d6ef62b72f5871b88640
Maria-hub746 <113303447+Maria-hub746@users.noreply.github.com>
2023-05-16 12:54:50 +0300

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(nums):
+    for i in range(1, len(nums)):
+        item_to_insert = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > item_to_insert:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = item_to_insert
+
+nums = [10, 1, 15, 26, 7, 9]
+insertion_sort(nums)
+print(nums)
